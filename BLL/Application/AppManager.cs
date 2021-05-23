c89d69b412500a6ff2f5512beafc7229b2a57225
Rajesh Narayan <rajesh.shetty@relyonsoft.com-at-583467868209>
2021-05-23 20:15:38 +0530

    declared new static bool variable to set sql connection opened or closed

--- BLL/Application/AppManager.cs
+++ BLL/Application/AppManager.cs
@@ -3,18 +3,29 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 
 namespace EasyBilling.BLL.Application
 {
     public class AppManager
     {
         public static SqlConnection ConnectionManager;
+        public static bool isConnectionOpened;
         public static void LaunchApplication()
         {
-            string connectionString;
-            connectionString = @"Data Source=NAVYA\RELYONDB2008R2;Initial Catalog=EasyBilling;User ID=sa;Password=reladmin@123;Integrated Security=false";
-            ConnectionManager = new SqlConnection(connectionString);
-            ConnectionManager.Open();
+            try
+            {
+                string connectionString;
+                connectionString = @"Data Source=NAVYA\RELYONDB2008R2;Initial Catalog=EasyBilling;User ID=sa;Password=reladmin@123;Integrated Security=false";
+                ConnectionManager = new SqlConnection(connectionString);
+                ConnectionManager.Open();
+                isConnectionOpened = true;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                isConnectionOpened = false;
+            }
         }
     }
 }
